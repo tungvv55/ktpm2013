08375d38a577b37b9926a35670d2e714c3caa195
tungvv55 <tungvv55@gmail.com>
2013-09-10 22:26:28 +0700

    BT1

---
+++ BT1/GPTB1.java
@@ -0,0 +1,19 @@
+//Giai phuong trinh bac 1
+public class GPTB1 
+{
+	public int test(int a, int b) 
+	{
+		if((a==0)&&(b==0))
+		{
+		printf("X vo han");
+		}
+		else
+		{
+			if((a==0)&&(b!=0))
+			{
+				printf("Khong co x");
+			}
+			return -b/a;
+		}
+	}
+}
---
+++ BT1/TestGPTB1.java
@@ -0,0 +1,18 @@
+// Test giai phuong trinh bac 1
+import static org.junit.*;
+
+public class TestGPTB1 
+{
+	private GPTB1 GPT = new GPTB1();
+	
+	public void test1() 
+	{
+		assertEquals("",-1, GPT.test(1, 1));
+	}
+	
+	public void test2() 
+	{
+		assertEquals("",9, GPT.test(-10, 90));
+	}
+
+}
